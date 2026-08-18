1dcd82cf44a215ce61dbdcbb1c288dece9417583
DuqueJR <duqueruizjeronimo@gmail.com>
2026-08-17 19:38:27 -0500

    fix(BST): track parent reference on insert

--- src/main/java/com/discoballplayer/model/structures/BST.java
+++ src/main/java/com/discoballplayer/model/structures/BST.java
@@ -1,35 +1,44 @@
 package main.java.com.discoballplayer.model.structures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BST <T extends Comparable<T>> {
 
     private static class Node<T extends Comparable<T>>{
         T value;
-        Node<T> left, rigth;
-        public Node(T value){
+        Node<T> left, rigth, parent;
+        public Node(T value, Node<T> parent){
             this.value = value;
+            this.parent = parent;
         }
     }
 
     Node<T> root;
 
     public void insert(T value){
-        root = insertr(root, value);
+
+        root = insertr(root, value, null);
     }
 
-    private Node insertr(Node<T> current, T value){
+    private Node insertr(Node<T> current, T value, Node<T> parent){
         if(current == null){
-            return new Node<T>(value);
+            return new Node(value,parent);
         }
 
         else if (value.compareTo(current.value) < 0){
-            current.left = insertr(current.rigth, value);
+            current.left = insertr(current.rigth, value, current);
         }
 
         else if (value.compareTo(current.value) > 0){
-            current.rigth = insertr(current.rigth, value);
+            current.rigth = insertr(current.rigth, value, current);
         }
 
         return current;
 
+    }
+
+    public boolean isEmpty() {
+        return root == null;
     }
 }
